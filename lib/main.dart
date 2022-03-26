e5d3b8601a82f492e7a742461453984225eb183f
unknown <you@example.com>
2022-03-26 14:52:43 +0300

    <product list added>

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:nkc/home/home.dart';
+import 'registration/login.dart';
+import 'registration/register.dart';
+import 'package:nkc/product_list.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => MyLogin(),
+        'registration': (context) => Registration(),
+        'product_list': (context) => Product_list()
+      }));
 }
